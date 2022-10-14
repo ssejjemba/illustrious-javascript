e25d34bce533819c8eee7a17f019efcf222d95ba
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-10-14 23:42:41 +0300

    Finish script for quadratic curve draws

--- sketch.js
+++ sketch.js
@@ -13,8 +13,10 @@ const dotsRadius = 10;
 
 const points = [
   new Circle({ x: 200, y: 540, radius: dotsRadius }),
-  new Circle({ x: 400, y: 300, radius: dotsRadius, control: true }),
+  new Circle({ x: 400, y: 300, radius: dotsRadius }),
   new Circle({ x: 880, y: 540, radius: dotsRadius }),
+  new Circle({ x: 600, y: 700, radius: dotsRadius }),
+  new Circle({ x: 640, y: 900, radius: dotsRadius }),
 ];
 /**
  * @param { canvas: HTMLCanvasElement }
@@ -27,27 +29,40 @@ const sketch = ({ canvas }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
+    context.strokeStyle = "#999";
     context.beginPath();
     context.moveTo(points[0].x, points[0].y);
-    context.quadraticCurveTo(
-      points[1].x,
-      points[1].y,
-      points[2].x,
-      points[2].y
-    );
+
+    for (let i = 1; i < points.length; i++) {
+      context.lineTo(points[i].x, points[i].y);
+    }
     context.stroke();
+
+    drawSeemlessCurve(context);
 
     points.forEach((point) => point.draw(context));
   };
 };
 
 const onDragStart = (place) => {
+  let hit = false;
   points.forEach((point) => {
     point.isDragging = isHitCircle(place, {
       radius: dotsRadius,
       center: point,
     });
+    if (!hit && point.isDragging) {
+      hit = true;
+    }
   });
+
+  if (!hit) {
+    addPointToPlace(place);
+  }
+};
+
+const addPointToPlace = (place) => {
+  points.push(new Circle({ x: place.x, y: place.y, radius: dotsRadius }));
 };
 
 /**
@@ -61,6 +76,34 @@ const onDrag = ({ x, y }) => {
       point.y = y;
     }
   });
+};
+
+/**
+ *
+ * @param {CanvasRenderingContext2D} context
+ */
+const drawSeemlessCurve = (context) => {
+  context.save();
+  context.beginPath();
+  for (let i = 0; i < points.length - 1; i++) {
+    const curr = points[i];
+    const next = points[i + 1];
+
+    const mx = curr.x + (next.x - curr.x) * 0.5;
+    const my = curr.y + (next.y - curr.y) * 0.5;
+    if (i === 0) {
+      context.moveTo(curr.x, curr.y);
+    }
+    if (i === points.length - 2) {
+      context.quadraticCurveTo(curr.x, curr.y, next.x, next.y);
+      continue;
+    }
+    context.quadraticCurveTo(curr.x, curr.y, mx, my);
+  }
+  context.lineWidth = 4;
+  context.strokeStyle = "blue";
+  context.stroke();
+  context.restore();
 };
 
 /**
