202b6b4abb1f6aa5cbeee4f61830626b14f597e5
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-10-15 02:59:48 +0300

    Add audio two visualiser

--- audio/audio2.js
+++ audio/audio2.js
@@ -8,100 +8,88 @@ const settings = {
   animate: true,
 };
 
-/**
- * @type {HTMLAudioElement}
- */
 let audio;
 let audioContext, audioData, sourceNode, analyserNode;
 let manager;
-let _width, _height;
 let minDb, maxDb;
 
-const numCircles = 5;
-const numSlices = 9;
-const slice = (Math.PI * 2) / numSlices;
-const radius = 200;
-
-const bins = [];
-const lineWidths = [];
-
-let lineWidth, bin, mapped;
-
-/**
- * @param { canvas: HTMLCanvasElement }
- * @returns {function({ context: CanvasRenderingContext2D, width: number, height: number})}
- */
 const sketch = () => {
+  const numCircles = 5;
+  const numSlices = 1;
+  const slice = (Math.PI * 2) / numSlices;
+  const radius = 200;
+
+  const bins = [];
+  const lineWidths = [];
+  const rotationOffsets = [];
+
+  let lineWidth, bin, mapped, phi;
+
   for (let i = 0; i < numCircles * numSlices; i++) {
     bin = random.rangeFloor(4, 64);
-    if (random.value() > 0.5) bin = 0;
     bins.push(bin);
   }
 
   for (let i = 0; i < numCircles; i++) {
     const t = i / (numCircles - 1);
-    lineWidth = eases.quadIn(t) * 200 + 20;
+    lineWidth = eases.quadIn(t) * 200 + 10;
     lineWidths.push(lineWidth);
   }
-  // audio.play();
+
+  for (let i = 0; i < numCircles; i++) {
+    rotationOffsets.push(
+      random.range(Math.PI * -0.25, Math.PI * 0.25) - Math.PI * 0.5
+    );
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = "#EEEAE0";
     context.fillRect(0, 0, width, height);
-    _width = width;
-    _height = height;
-    createVisualisation(context);
-  };
-};
 
-/**
- *
- * @param {CanvasRenderingContext2D} context
- */
-const createVisualisation = (context) => {
-  if (!audioContext) return;
+    if (!audioContext) return;
 
-  analyserNode.getFloatFrequencyData(audioData);
+    analyserNode.getFloatFrequencyData(audioData);
 
-  context.save();
-  context.translate(_width * 0.5, _height * 0.5);
-
-  let cradius = radius;
-
-  for (let i = 0; i < numCircles; i++) {
     context.save();
+    context.translate(width * 0.5, height * 0.5);
+    context.scale(1, -1);
 
-    for (let j = 0; j < numSlices; j++) {
-      context.rotate(slice);
-      context.lineWidth = lineWidths[i];
+    let cradius = radius;
 
-      bin = bins[i * numSlices + j];
-      if (!bin) continue;
+    for (let i = 0; i < numCircles; i++) {
+      context.save();
+      context.rotate(rotationOffsets[i]);
 
-      mapped = math.mapRange(audioData[bin], minDb, maxDb, 0, 1, true);
+      cradius += lineWidths[i] * 0.5 + 2;
 
-      lineWidth = lineWidths[i] * mapped;
-      if (lineWidth < 1) continue;
+      for (let j = 0; j < numSlices; j++) {
+        context.rotate(slice);
+        context.lineWidth = lineWidths[i];
 
-      context.lineWidth = lineWidth;
+        bin = bins[i * numSlices + j];
 
-      context.beginPath();
-      context.arc(0, 0, cradius + context.lineWidth * 0.5, 0, slice);
-      context.stroke();
+        mapped = math.mapRange(audioData[bin], minDb, maxDb, 0, 1, true);
+
+        phi = slice * mapped;
+
+        context.beginPath();
+        context.arc(0, 0, cradius, 0, phi);
+        context.stroke();
+      }
+
+      cradius += lineWidths[i] * 0.5;
+
+      context.restore();
     }
 
-    cradius += lineWidths[i];
-
     context.restore();
-  }
-
-  context.restore();
+  };
 };
 
 const addListeners = () => {
   window.addEventListener("mouseup", () => {
-    if (!audioContext) {
-      createAudio();
-    }
+    if (!audioContext) createAudio();
+
     if (audio.paused) {
       audio.play();
       manager.play();
@@ -114,30 +102,40 @@ const addListeners = () => {
 
 const createAudio = () => {
   audio = document.createElement("audio");
+  // The audio file is not part of the repository and has to be donwloaded separately
   audio.src = "../assets/WEARETHEGOOD - Live in the Moment.mp3";
 
   audioContext = new AudioContext();
+
   sourceNode = audioContext.createMediaElementSource(audio);
   sourceNode.connect(audioContext.destination);
 
   analyserNode = audioContext.createAnalyser();
+  analyserNode.fftSize = 512;
   analyserNode.smoothingTimeConstant = 0.9;
   sourceNode.connect(analyserNode);
+
   minDb = analyserNode.minDecibels;
   maxDb = analyserNode.maxDecibels;
 
   audioData = new Float32Array(analyserNode.frequencyBinCount);
+
+  // console.log(audioData.length);
 };
 
 const getAverage = (data) => {
-  const sum = data.reduce((prev, curr) => prev + curr, 0);
+  let sum = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    sum += data[i];
+  }
+
   return sum / data.length;
 };
 
 const start = async () => {
   addListeners();
   manager = await canvasSketch(sketch, settings);
-
   manager.pause();
 };
 
